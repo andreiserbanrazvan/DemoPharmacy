44d336a035f496ef408a2d424a716294f8366f2a
DESKTOP-6LTCLPR\andre <andreiserbanrazvan@gmail.com>
2020-09-16 14:49:55 +0200

    CrudDummy

--- DemoApp/Startup.cs
+++ DemoApp/Startup.cs
@@ -58,7 +58,7 @@ namespace DemoApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Employees}/{action=Index}/{id?}");
             });
         }
     }
